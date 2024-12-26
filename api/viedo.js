490855f2f621aa9a0a71d22a53a603500ea93395
Ruta Mavani <rutamavani01@gmail.com>
2024-12-26 16:41:23 +0530

    ssss

--- api/viedo.js
+++ api/viedo.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 const app = express();
 
-app.get('/videos', async (req, res) => {
+app.get('/api/viedo', async (req, res) => {
     try {
         const categoryId = req.query.category_id || '33'; // Default to category_id=33 if not provided
         const response = await axios.get(`https://p9videos.com/master/api/v2/getvideos?category_id=${categoryId}`);
